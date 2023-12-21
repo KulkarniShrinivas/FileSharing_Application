4460fb2ab8a528589092a7ecaef1dbb782f3c5b7
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-21 23:39:15 +0530

    created routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 //const express  = require('express') old new we can import in EA6
 
 import express from 'express';
+import router from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use('/',router);
 
 const PORT = 8000;
 
---
+++ server/routes/routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { uploadImage } from '../controller/image-controller.js';
+
+
+const router = express.Router();
+
+router.post('/upload',  uploadImage)
+
+export default router;
